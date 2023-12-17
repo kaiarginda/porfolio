0730abccfea255df6e42328de2e36d629d02facc
Your Name <your.email@example.com>
2023-12-17 10:07:34 -0800

    first commit

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,17 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, webUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
-          <h4>{title}</h4>
+          <h4>
+            <a href={webUrl}>{title}</a>
+          </h4>
           <span>{description}</span>
         </div>
       </div>
     </Col>
-  )
-}
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,36 +14,42 @@ export const Projects = () => {
       description: "Nextjs",
       imgUrl:
         "https://road-blue.vercel.app/_next/static/media/header-bg.2ed11f7f.jpg",
+      webUrl: "https://road-blue.vercel.app/",
     },
     {
       title: "Wine Website",
       description: "Nextjs",
       imgUrl:
         "https://winerestaurant-p9gp.vercel.app/_next/static/media/section-1-bg.64d7356a.jpg",
+      webUrl: "https://winerestaurant-p9gp.vercel.app/",
     },
     {
       title: "Shop Website",
       description: "Nextjs",
       imgUrl:
         "https://scontent.xx.fbcdn.net/v/t1.15752-9/403394942_1382829645655115_394241643674106534_n.png?stp=dst-png_p206x206&_nc_cat=110&ccb=1-7&_nc_sid=510075&_nc_ohc=Y_NZ99F4cJYAX-BiHhc&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdS8f_XMJ1MkxO40BXrygRsL0NtJEZR-ITfArDkSWcUyOw&oe=659BBB6E",
+      webUrl: "https://shop-7l9b.vercel.app/",
     },
     {
       title: "Movie Website",
       description: "MERN",
       imgUrl:
         "https://scontent.xx.fbcdn.net/v/t1.15752-9/410491649_702407755327575_8428482639608542065_n.png?stp=dst-png_p206x206&_nc_cat=101&ccb=1-7&_nc_sid=510075&_nc_ohc=OjMrRIAueKAAX8Z8edY&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQh5O61ILmJuVc9rSXU7wfTF3yfWufV2E_1tVd2QPxukA&oe=65A1424D",
+      webUrl: "https://mov-dv4y.onrender.com/",
     },
     {
       title: "Porfolio Website",
       description: "Nextjs",
       imgUrl:
         "https://scontent.xx.fbcdn.net/v/t1.15752-9/398411024_871977677724491_623173634656328917_n.png?stp=dst-png_p206x206&_nc_cat=105&ccb=1-7&_nc_sid=510075&_nc_ohc=RgyZC1m8MYgAX8jckOu&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdRiyaAsU0aWcCPTHFI7MYFhXTCqjVMnDHHHC2iAvzFSvQ&oe=65A692F0",
+      webUrl: "https://portfolio-lteg.vercel.app/",
     },
     {
       title: "Hoobank",
       description: "React",
       imgUrl:
         "https://scontent.xx.fbcdn.net/v/t1.15752-9/403416177_737098791184754_3135171421297123451_n.png?stp=dst-png_p206x206&_nc_cat=105&ccb=1-7&_nc_sid=510075&_nc_ohc=SkerYS0vTcMAX9rdi42&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQ-ocqYJJdh3KKis7zgEsiCWWpffkVlzedP_Uk3OmCQyQ&oe=65A67B02",
+      webUrl: "https://hoobankk.onrender.com/",
     },
   ];
 
